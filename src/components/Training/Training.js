7649b0527e4f345de2e63f0d048f98d8e6a49e7c
Margarita Shishkova <margolen239@gmail.com>
2022-11-21 23:26:02 +0300

    create cards

--- src/components/Training/Training.js
+++ src/components/Training/Training.js
@@ -1,45 +1,86 @@
 import Card from "../Card/Card";
 import "./Training.scss";
-import {useState} from 'react';
+import { useState } from "react";
 
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
+  let currentIndex = array.length,
+    randomIndex;
 
   // While there remain elements to shuffle.
   while (currentIndex !== 0) {
-
     // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
     // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
 
   return array;
 }
 
+function Training({ words }) {
+  const shuffledWords = shuffle(words);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [disabled, setDisabled] = useState(false);
 
-function Training({words}) {
-    const shuffledWords = shuffle(words)
-    const [currentIndex, setCurrentIndex] = useState(0)
-    const [disabled, setDisabled] = useState(false)
-    function nextCard() {
-        if (currentIndex + 2 <= shuffledWords.length) {
-            setCurrentIndex(currentIndex + 1)
-        } else {
-            setDisabled(true)
-        }
+  function nextCard() {
+    if (currentIndex + 1 < shuffledWords.length) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      setDisabled(true);
     }
-    return (
-        <div className="training-card">
+  }
 
-            <Card word={shuffledWords[currentIndex]}/>
-            <button onClick={nextCard} disabled={disabled}>Next</button>
-        </div>
-    )
+  function prevCard() {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    } else {
+      setDisabled(true);
+    }
+  }
+  return (
+    <div className="training-card">
+      <button onClick={prevCard} disabled={disabled}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-arrow-left-circle"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
+          />
+        </svg>
+      </button>
+      <Card word={shuffledWords[currentIndex]} />
+      <button onClick={nextCard} disabled={disabled}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-arrow-right-circle"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
+          />
+        </svg>
+      </button>
+      <div className="counter">
+        {currentIndex}/{shuffledWords.length}
+      </div>
+    </div>
+  );
 }
 
 export default Training;
